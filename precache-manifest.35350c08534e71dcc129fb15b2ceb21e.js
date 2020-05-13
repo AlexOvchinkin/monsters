d493fed3c87035735de0bdbaafc484a4aaf2301f
Алексей Овчинкин <al.ovchinkin@gmail.com>
2020-05-12 23:42:43 -0700

    Updates

---
+++ precache-manifest.35350c08534e71dcc129fb15b2ceb21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97162ad636a9871524271c7e5ee10487",
+    "url": "/AlexOvchinkin/monsters/index.html"
+  },
+  {
+    "revision": "bce7c09c984b1ad318e0",
+    "url": "/AlexOvchinkin/monsters/static/css/main.6a14798f.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/AlexOvchinkin/monsters/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlexOvchinkin/monsters/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce7c09c984b1ad318e0",
+    "url": "/AlexOvchinkin/monsters/static/js/main.30924226.chunk.js"
+  },
+  {
+    "revision": "0998927f71078cb78cf9",
+    "url": "/AlexOvchinkin/monsters/static/js/runtime-main.a3dfa16f.js"
+  }
+]);
